ce50520eb6db0764faf3e8fec50fbc3052fd4a48
Mi chael <51323744+mjk1999@users.noreply.github.com>
2023-08-05 00:45:18 +0800

    Update chatai.js

--- ycdz/chatai.js
+++ ycdz/chatai.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/chatai.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/mjk1999/Quantumult-X/main/ycdz/chatai.js
 
 [mitm]
 
